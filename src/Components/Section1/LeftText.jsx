c35dd0206f6568543df331df22a2ccccdfb1be6a
Akshita Gupta <gungunakshita37gupta973@gmail.com>
2025-10-18 14:57:03 +0530

    creating the left side

--- src/Components/Section1/LeftText.jsx
+++ src/Components/Section1/LeftText.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import { MoveUpRight } from 'lucide-react';
 
 function LeftText() {
   return (
-    <div className='h-full w-1/3 bg-gray-50'>
-        
+    <div className='flex flex-col justify-between h-[70vh] w-1/3 bg-gray-50'>
+      <div className='p-6'>
+        <h3 className=' leading-[1.2] mb-8 text-6xl font-bold'>Prospective <br /> <span>Customer</span> <br /> <span> Segmentation</span></h3>
+        <p className='text-lg font-thin text-gray-800'>Success means achieving your goals with hard work, discipline, and focus.
+          It is not just winning but also learning from failures</p>
+      </div>
+      <div className='text-8xl'>
+        <MoveUpRight />
+      </div>
     </div>
   )
 }
--- src/Components/Section1/Page1Content.jsx
+++ src/Components/Section1/Page1Content.jsx
@@ -4,7 +4,7 @@ import RightText from './RightText'
 
 function Page1Content() {
   return (
-    <div className='px-8 pt-10 h-[90vh] flex  bg-gray-400 gap-12 py-18'>
+    <div className='px-8 pt-10 h-[90vh] flex  bg-gray-100 gap-12 py-18'>
         <LeftText />
         <RightText />
     </div>
